e1cc6b5bde2c348996dbd6761d64eda256e66425
balyaGround <balyazulfikar@gmail.com>
2022-03-17 16:32:57 +0700

    mau nambah isi web theming

--- src/pages/webTheming/webTheming.jsx
+++ src/pages/webTheming/webTheming.jsx
@@ -9,21 +9,15 @@ function WebTheming() {
         <div className="row " style={{ height: "100vh" }}>
           <NavBar />
           <div className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div className="chartjs-size-monitor">
-              <div className="chartjs-size-monitor-expand">
-                <div className=""></div>
-              </div>
-              <div className="chartjs-size-monitor-shrink">
-                <div className=""></div>
-              </div>
-            </div>
             <div className="d-flex justify-content-between flex-column flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
               <h1 className="h2">Web Theming</h1>
             </div>
-
-            {/* <canvas className="my-4 w-100 chartjs-render-monitor" id="myChart" width="2196" height="926" style="display: block; width: 1098px; height: 463px;"></canvas> */}
-
             <h2>Theming Console</h2>
+            <div className="row">
+              <div className="col">
+                <h5>Haloooo</h5>
+              </div>
+            </div>
           </div>
         </div>
       </div>
